1c1bf1c1d15df1544b84c86eda99b888fed107c2
Davis <davisgalla946@gmail.com>
2025-01-07 14:15:50 +0200

    parammetri

--- index.php
+++ index.php
@@ -5,10 +5,27 @@ require "Database.php";
 $config = require "config.php";
 
 $db = new Database($config["database"]);
-$posts = $db->query("SELECT * FROM posts")->fetchAll();
+$select = "SELECT * FROM posts";
+$params = [];
+if (isset($_GET['search']) && $_GET['search']!=""){
+    $search_query = "%". $_GET['search']. "%";
+    $select .= " WHERE content LIKE :nosaukums";
+    $params = ["nosaukums" => $search_query];
+}
+
+    $posts = $db->query($select, $params)->fetchAll();
 
 
+ 
+ 
+echo "<form>";
+echo "<input name='search' />";
+echo "<button>Meklēt</button>";
+echo "</form>";
 
+if (count($posts) == 0){
+    echo " Nav atrsts";
+}
 
 echo "<ul>";
 foreach($posts as $post){
